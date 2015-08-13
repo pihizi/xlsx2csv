9dd7458199a30767950595a22cbf6c8f63bd093a
stenote <rui.ma@geneegroup.com>
2015-08-13 06:51:04 +0000

    使用 alpine:3.2 替换 debian:8, build 后镜像仅 48.91 MB

--- start.sh
+++ start.sh
@@ -1,7 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+
 srcDIR=/data/upload/
 tmpDIR=/data/tmp/
 destDIR=/data/parsed/
+
+if [ ! -d $srcDIR ]; then
+    mkdir -p $srcDIR
+fi
+
+if [ ! -d $tmpDIR ]; then
+    mkdir -p $tmpDIR
+fi
+
+if [ ! -d $destDIR ]; then
+    mkdir -p $destDIR
+fi
+
 /usr/bin/inotifywait -mrq --format '%f' -e create ${srcDIR} | while read file
 do
     tmp=${tmpDIR}${file%%.*}/
