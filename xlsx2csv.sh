fed59d5b167ebc3521f674eff8fc3aeae9b77830
pihizi <pihizi@msn.com>
2015-08-13 12:57:03 +0800

    .

--- xlsx2csv.sh
+++ xlsx2csv.sh
@@ -6,6 +6,7 @@ destDIR=/data/parsed/
 do
     tmp=${tmpDIR}${file%%.*}/
 	mkdir -p ${tmp}
+    echo "Start to convert: ${srcDIR}${file}"
 	xlsx2csv -a -i ${srcDIR}${file} ${tmp}
     if [ "$(ls -v ${tmp}) 2> /dev/null)" == "" ]; then
         echo "Failed to convert: ${srcDIR}${file}"
